4bbd11e08a86e12cf3cac61ad961a70beaed84a0
mingchen62 <mingchen62@gmail.com>
2019-09-09 10:52:09 -0400

    ignore email without body text(image or html)

--- src/transformer-compliance.py
+++ src/transformer-compliance.py
@@ -316,8 +316,9 @@ def evaluate(model, tokenizer, prefix=""):
     results.update(result)
 
     output_eval_file = os.path.join(eval_output_dir, "eval_results.txt")
-    with open(output_eval_file, "w") as writer:
+    with open(output_eval_file, "a") as writer:
         logger.info("***** Eval results {} *****".format(prefix))
+        writer.write("***** Eval results {} *****\n".format(prefix))
         for key in sorted(result.keys()):
             logger.info("  %s = %s", key, str(result[key]))
             writer.write("%s = %s\n" % (key, str(result[key])))
